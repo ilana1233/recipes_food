92868dd986cb53224583ae96a294bfa2e821cc0c
ilana_trabelsi <ilana221eilat@gmail.com>
2025-06-27 23:50:51 +0300

    first

--- server/server.js
+++ server/server.js
@@ -2,18 +2,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+require('dotenv').config();
 
 const authRoutes = require('./routes/authRoutes');
 const recipeRoutes = require('./routes/recipeRoutes');
 
-
 const app = express();
+const PORT = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://ilana221eilat:Aa1234567@cluster0.mtgqysu.mongodb.net')
-.then(() => console.log('connect to mongo!'))
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifieldTopology: true,
+}).then(() => console.log('connect to mongo!'))
 .catch(err => console.error('שגיאה בחיבור למונגו:', err));
+
+app.get('/', (req,res) => {
+    res.send('Server is running');
+});
   
 
 app.use('/api/auth', authRoutes);
@@ -23,6 +31,6 @@ app.use(express.static(patch.join(__dirname, '../client/build')));
 
 
 app.listen(5000, () => {
-  console.log('🔥 Server running on http://localhost:5000');
+  console.log(`🔥 Server running on port ${PORT}`);
 });
 
