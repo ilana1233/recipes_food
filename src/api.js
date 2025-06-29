4988a0c7606bc0b93338b61d088b9bb476f05b33
ilana_trabelsi <ilana221eilat@gmail.com>
2025-06-29 18:09:58 +0300

    change

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
+// src/api.js
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:
-    process.env.REACT_APP_API_URL ||
-    'https://recipes-food-i2xb.onrender.com/api',});
+  baseURL: process.env.REACT_APP_API_URL || "https://recipes-food-i2xb.onrender.com/api",
+});
 
 export default api;
