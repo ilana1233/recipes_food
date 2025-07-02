fc26c2a8ea7ed7eacc55f4f40390526bcebbabd9
ilana_trabelsi <ilana221eilat@gmail.com>
2025-07-03 00:09:08 +0300

    שינוי

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe');
 const cors = require('cors');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 
 const path = require('path');
 require('dotenv').config();
