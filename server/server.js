a7c97a6cd3f17e4d43af2eb1613a19fdd3e42136
ilana_trabelsi <ilana221eilat@gmail.com>
2025-07-02 23:13:14 +0300

    שינוי

--- server/server.js
+++ server/server.js
@@ -3,8 +3,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
-console.log(bcrypt);
+const bcryptjs = require('bcryptjs');
+console.log(bcryptjs)
+
 const path = require('path');
 require('dotenv').config();
 
@@ -21,8 +22,7 @@ app.use(express.json());
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+ 
 }).then(() => console.log('✅ Connected to MongoDB'))
   .catch(err => console.error('❌ MongoDB connection error:', err));
 
