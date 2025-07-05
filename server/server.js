d9cf646344422863653ebcdead8bf4b87d653707
ilana_trabelsi <ilana221eilat@gmail.com>
2025-07-05 13:23:27 +0300

    הוספת תיקיית server עם קוד השרת

--- server/server.js
+++ server/server.js
@@ -12,18 +12,12 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin:"http://recipe-food-yvy7.onrender.com",
-    credentials: true
+    origin: '*' ,credentials:true
 }));
-// app.options('*', cors());
-
 app.use(express.json());
-
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
 }).then(() => console.log('✅ Connected to MongoDB'))
   .catch(err => console.error('❌ MongoDB connection error:', err));
 
